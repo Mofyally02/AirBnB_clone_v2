b8bd83777adee6ea0bf8984786f3d0b40a0b9fce
Mofyally02 <mofyally02@gmail.com>
2023-06-22 18:19:19 +0300

    Update

--- models/review.py
+++ models/review.py
@@ -1,23 +1,10 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from sqlalchemy import Column, String, ForeignKey
-
-from models.base_model import BaseModel, Base
+from models.base_model import BaseModel
 
 
-class Review(BaseModel, Base):
-    """ Review class to store review information """
-
-    __tablename__ = "reviews"
-
-    text = Column(String(1024), nullable=False)
-    user_id = Column(
-        String(60),
-        ForeignKey("users.id", ondelete="CASCADE"),
-        nullable=False
-    )
-    place_id = Column(
-        String(60),
-        ForeignKey("places.id", ondelete="CASCADE"),
-        nullable=False
-    )
+class Review(BaseModel):
+    """ Review classto store review information """
+    place_id = ""
+    user_id = ""
+    text = ""
